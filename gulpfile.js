26ac2c708bb0d0f1866649e85f731c6529cac855
Ryan Atkinson <rjatkinson2@gmail.com>
2015-06-10 00:18:19 -0700

    (feat) Implemented base HTML structure and updated gulp to handle CSS/Images

--- gulpfile.js
+++ gulpfile.js
@@ -16,20 +16,32 @@ var path = {
   TEST_SRC: '__tests__/*.js',
   HTML: 'client/index.html',
   MINIFIED_OUT: 'build.min.js',
+  CSS: 'client/css/*',
+  IMAGE: 'client/img/*',
   OUT: 'build.js',
   DEST: 'dist/client',
   DEST_BUILD: 'dist/build',
   DEST_SRC: 'dist/client',
+  DEST_STYLE: 'dist/client/style',
   ENTRY_POINT: './client/js/App.js'
 };
 
 // Copies HTML file, replaces script source with the bundled source.
 gulp.task('replaceHTMLsrc', function(){
+  console.log('transfering HTML file and swapping source with bundle.js');
   gulp.src(path.HTML)
     .pipe(htmlreplace({
       'js': path.OUT
     }))
     .pipe(gulp.dest(path.DEST));
+});
+
+// Transfers CSS and Image folders over to distribution.
+// This will temporarily happen without any without any processing.
+gulp.task('transferStyles', function(){
+  console.log('transfering styles to distribution folder');
+  gulp.src([path.CSS, path.IMAGE])
+    .pipe(gulp.dest(path.DEST_STYLE));
 });
 
 // watches HTML and JS for changes, browserify and reactifys JS, and bundles it all.
@@ -70,7 +82,7 @@ gulp.task('watchTests', function(){
   gulp.watch([path.CLIENT_JS, path.TEST_SRC], ['jest']);
 });
 
-gulp.task('default', ['replaceHTMLsrc', 'watch']);
+gulp.task('default', ['replaceHTMLsrc', 'transferStyles', 'watch']);
 
 // Version 1.2.0 of gulp-uglify uses a breaking version of UglifyJS (2.4.19)
 // We can update this once we start needing to deal with production
