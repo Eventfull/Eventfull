ee7aa62be3c7de66e65f97c02c524f0d55527c81
Joshua Newman <jonewman1020@gmail.com>
2015-06-11 20:36:44 -0700

    (mocks) Create expected mock month data

---
+++ client/js/monthData.js
@@ -0,0 +1,178 @@
+module.exports = {
+  'week1': [
+    {
+      '05012015': {
+        numEvents: 3,
+        status: 'good'
+      }
+    },
+    {
+      '05022015': {
+        numEvents: 3,
+        status: 'good'
+      }
+    },
+    {
+      '05032015': {
+        numEvents: 3,
+        status: 'good'
+      }
+    },
+    {
+      '05042015': {
+        numEvents: 3,
+        status: 'good'
+      }
+    },
+    {
+      '05052015': {
+        numEvents: 3,
+        status: 'good'
+      }
+    },
+    {
+      '05062015': {
+        numEvents: 3,
+        status: 'good'
+      }
+    },
+    {
+      '05072015': {
+        numEvents: 3,
+        status: 'good'
+      }
+    }
+  ],
+  'week2': [
+    {
+      '05082015': {
+        numEvents: 3,
+        status: 'good'
+      }
+    },
+    {
+      '05092015': {
+        numEvents: 3,
+        status: 'good'
+      }
+    },
+    {
+      '05102015': {
+        numEvents: 3,
+        status: 'good'
+      }
+    },
+    {
+      '05112015': {
+        numEvents: 3,
+        status: 'good'
+      }
+    },
+    {
+      '05122015': {
+        numEvents: 3,
+        status: 'good'
+      }
+    },
+    {
+      '05132015': {
+        numEvents: 3,
+        status: 'good'
+      }
+    },
+    {
+      '05142015': {
+        numEvents: 3,
+        status: 'good'
+      }
+    }
+  ],
+  'week3': [
+    {
+      '05152015': {
+        numEvents: 3,
+        status: 'bad'
+      }
+    },
+    {
+      '05162015': {
+        numEvents: 3,
+        status: 'bad'
+      }
+    },
+    {
+      '05172015': {
+        numEvents: 3,
+        status: 'bad'
+      }
+    },
+    {
+      '05182015': {
+        numEvents: 3,
+        status: 'bad'
+      }
+    },
+    {
+      '05192015': {
+        numEvents: 3,
+        status: 'bad'
+      }
+    },
+    {
+      '05202015': {
+        numEvents: 3,
+        status: 'bad'
+      }
+    },
+    {
+      '05212015': {
+        numEvents: 3,
+        status: 'bad'
+      }
+    },
+  ],
+  'week4': [
+    {
+      '05222015': {
+        numEvents: 10,
+        status:'good'
+      }
+    },
+    {
+      '05232015': {
+        numEvents: 10,
+        status:'good'
+      }
+    },
+    {
+      '05242015': {
+        numEvents: 10,
+        status:'good'
+      }
+    },
+    {
+      '05252015': {
+        numEvents: 10,
+        status:'good'
+      }
+    },
+    {
+      '05262015': {
+        numEvents: 10,
+        status:'good'
+      }
+    },
+    {
+      '05272015': {
+        numEvents: 10,
+        status:'good'
+      }
+    },
+    {
+      '05282015': {
+        numEvents: 10,
+        status:'good'
+      }
+    },
+  ]
+};
