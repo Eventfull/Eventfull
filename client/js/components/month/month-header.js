e4f5c0773364af5150f35874cab51dac20c6ba00
Joshua Newman <jonewman1020@gmail.com>
2015-06-11 20:07:09 -0700

    (navigation) Move week link to month-header from month

--- client/js/components/month/month-header.js
+++ client/js/components/month/month-header.js
@@ -5,6 +5,7 @@ var MonthHeader = React.createClass({
   render:function() {
     return (
       <div>
+        <Link href='/week'>Week View</Link>
         <h3>Im the Month Header</h3>
       </div>
     );
