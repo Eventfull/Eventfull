9217fb69ed32aca9907fb13cfc5455639735d74b
Joshua Newman <jonewman1020@gmail.com>
2015-06-13 13:57:49 -0700

    (store) Set up scaffolding for month flux architecture

--- client/js/stores/month-store.js
+++ client/js/stores/month-store.js
@@ -1,9 +1,34 @@
-var Dispatcher = require('./dispatcher/dispatcher');
-var AppConstants = require('./constants/constants');
-var EventEmitter = require('event').EventEmitter;
+var Dispatcher = require('../dispatcher/dispatcher');
+var AppConstants = require('../constants/constants');
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
 
-var MonthStore = {
+//Internal storage containing all data for a given month
+var _monthData = {};
 
+//setter method for the _monthData internal storage
+var _setMonthData = function(data) {
+  _monthData = data;
 };
+
+var MonthStore = assign({}, EventEmitter.prototype, {
+  getMonthData: function() {
+    return _monthData;
+  },
+
+  emitChange: function() {
+    this.emit('change');
+  },
+
+  addChangeListener: function(callback) {
+    this.on('change', callback);
+  },
+
+  removeChangeListener: function(callback) {
+    this.removeListener('change', callback);
+  }
+});
+
+
 
 module.exports = MonthStore;
