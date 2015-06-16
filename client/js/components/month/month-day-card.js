dd13459998da0b2c66893eb545347bb0deed9146
Joshua Newman <jonewman1020@gmail.com>
2015-06-16 14:37:59 -0700

    (feat) Add ability to route to specific weeks and days via react-router from month component

--- client/js/components/month/month-day-card.js
+++ client/js/components/month/month-day-card.js
@@ -1,13 +1,17 @@
 /** @jsx React.DOM */
 var React = require('react');
+var Link = require('react-router-component').Link;
 
 var MonthDayCard = React.createClass({
   render:function () {
+    var currentDate = this.props.day.date;
+    var dayPath = '/day/' + currentDate;
     return (
       <div>
         <p>Date: { this.props.day.date }</p>
         <p>Number of Events: { this.props.day.numEvents }</p>
         <p>Status: { this.props.day.status } </p>
+        <Link href={dayPath}>Go to this Day</Link>
       </div>
     );
   }
