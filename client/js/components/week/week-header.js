8f5b708655e0fa823ea7355dc5531e1f2cd1ad09
Ryan Atkinson <rjatkinson2@gmail.com>
2015-06-17 21:59:20 -0700

    (feat) Disable elastic overscroll in chrome, safari, and firefox

--- client/js/components/week/week-header.js
+++ client/js/components/week/week-header.js
@@ -6,7 +6,7 @@ var WeekHeader = React.createClass({
     return (
       <div className="date-bar">
         <div className="arrow arrow-left"></div>
-        <h2>May 25</h2>
+        <h2>May 25 - May 30</h2>
         <div className="arrow arrow-right"></div>
       </div>
     );
