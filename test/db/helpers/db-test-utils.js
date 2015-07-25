6c9971f17852c1e72b1640b4c0fbb6d36be32e62
benignbemine <kyle.17.owen@gmail.com>
2015-07-24 22:32:41 -0700

    (refactor) wait for db sync to run tests

--- test/db/helpers/db-test-utils.js
+++ test/db/helpers/db-test-utils.js
@@ -1,22 +1,22 @@
 var _ = require("underscore");
-var models = require('../../../server/server.js').get('models');
-
 // accepts array of strings which represent table names
-// clears all records from those tables
+// clears all records from those table
 
-var clearTables = function(tables){
-  return models.sequelize.Promise.all(_.map(tables, function(table){
-      return models[table].destroy({
-        where: {
-          id: {
-            $ne: null
+module.exports = function(models){
+  var clearTables = function(tables){
+    return models.sequelize.Promise.all(_.map(tables, function(table){
+        return models[table].destroy({
+          where: {
+            id: {
+              $ne: null
+            }
           }
-        }
-      });
-    })
-  );
-}
+        });
+      })
+    );
+  }
 
-module.exports = {
- clearTables: clearTables
+  return {
+   clearTables: clearTables
+  };
 };
